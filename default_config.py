a14a602c8aeb0334e7b06d48b91d535eb31cc76e
Kenneth Lyons <ixjlyons@gmail.com>
2015-02-09 10:40:45 -0800

    Going with standard data structures rather than config classes.

--- default_config.py
+++ default_config.py
@@ -1,61 +1,47 @@
-import os
-
-data_path = os.path.join(
-    os.path.expanduser('~'), 'pygesture-data')
-vrep_path = os.path.join(
-    os.path.expanduser('~'), 'usr', 'v-rep', 'v-rep-3.2.0')
-
-# these classes are templates which will go in a config module
-class Sensor:
-    def __init__(self, channel, label='', name=''):
-        self.channel = channel
-        self.label = label
-        self.name = name
-
-class Gesture:
-    def __init__(self, label, abbrv='', name=''):
-        self.label = label
-        self.name = name
-        self.abbrv = abbrv
-
-sensors = [
-    Sensor(0, 'ECR', 'extensor carpi radialis brevis'),
-    Sensor(1, 'ED', 'extensor digitorum'),
-    Sensor(2, 'EPL', 'extensor pollicis longus'),
-    Sensor(3, 'FDS', 'flexor digitorum superficialis'),
-    Sensor(4, 'FCR', 'flexor carpi radialis'),
-    Sensor(5, 'PT', 'pronator teres')
-]
-
-# local variables not picked up by config
 fs = 4096
+input_range = 2
+trigger_rate = 8
 downsample_factor = 2
 
-recorder = Recorder(
+data_path = '~/pygesture-data'
+vrep_path = '~/usr/v-rep/v-rep-3.2.0'
+
+# channel_number: ('abbrv', 'description')
+sensors = {
+    0: ('ECR', 'extensor carpi radialis brevis'),
+    1: ('ED', 'extensor digitorum'),
+    2: ('EPL', 'extensor pollicis longus'),
+    3: ('FDS', 'flexor digitorum superficialis'),
+    4: ('FCR', 'flexor carpi radialis'),
+    5: ('PT', 'pronator teres')
+}
+
+recorder = dict( 
     fs=fs,
-    channels=channels 
+    channels=list(sensors.keys()),
+    trigger_rate=trigger_rate,
+    input_range=input_range
+)
+
+processor = dict(
+    input_fs=fs,
+    downsample_factor=2,
+    bandpass_filter=dict(
+        fc=(10, 450),
+        order=4
     )
+)
 
-processor = Processor(
-        input_fs=fs,
-        conditioning=[
-            BandpassFilter((10, 450), 4),
-            Downsampler(downsample_factor)
-        ]
-    )
-
-
-# Gesture(label, description)
-gestures = [
-    Gesture('l0', 'NC', 'no-contraction'),
-    Gesture('l1', 'CF', 'closed-fist'),
-    Gesture('l2', 'FP', 'forearm-pronation'),
-    Gesture('l3', 'FS', 'forearm-supination'),
-    Gesture('l4', 'OH', 'open-hand'),
-    Gesture('l5', 'RD', 'radial-deviation'),
-    Gesture('l6', 'TE', 'thumb-extension'),
-    Gesture('l7', 'UD', 'ulnar-deviation'),
-    Gesture('l8', 'WE', 'wrist-extension'),
-    Gesture('l9', 'UD', 'wrist-flexion')
-]
-
+# 'label': ('abbrv', 'description')
+gestures = {
+    'l0': ('NC', 'no-contraction'),
+    'l1': ('CF', 'closed-fist'),
+    'l2': ('FP', 'forearm-pronation'),
+    'l3': ('FS', 'forearm-supination'),
+    'l4': ('OH', 'open-hand'),
+    'l5': ('RD', 'radial-deviation'),
+    'l6': ('TE', 'thumb-extension'),
+    'l7': ('UD', 'ulnar-deviation'),
+    'l8': ('WE', 'wrist-extension'),
+    'l9': ('UD', 'wrist-flexion')
+}
