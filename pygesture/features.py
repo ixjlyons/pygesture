7dfa5d3fcbe3291ce69b7905c248cea9363fa90f
Kenneth Lyons <ixjlyons@gmail.com>
2015-02-13 23:07:01 -0800

    Turned feature functions into classes, which should make for a simple way to specify features in config.

--- pygesture/features.py
+++ pygesture/features.py
@@ -1,174 +1,145 @@
 import numpy as np
 
 
-def mav(x):
+class MAV:
     """
     Calculates the mean absolute value of a signal.
-
-    Parameters
-    ----------
-    x : ndarray
-        The signal (can be multidimensional, in which case the MAV is
-        calculated along columns)
-
-    Returns
-    -------
-    y : ndarray
-        The mean absolute value of each channel of the input.
     """
-    y = np.mean(np.absolute(x), 0)
-    return y
+
+    def __init__(self):
+        pass
+
+    def compute(self, x):
+        y = np.mean(np.absolute(x), axis=0)
+        return y
 
 
-def wl(x):
+class WL:
     """
     Calculates the waveform length of a signal. Waveform length is just the
     sum of the absolute value of all deltas (between adjacent taps) of a
     signal.
-
-    Parameters
-    ----------
-    x : ndarray
-        The raw signal(s) to calculate WL for. If multidimensional, WL is
-        calculated along columns.
-
-    Returns
-    -------
-    y : ndarray
-        The waveform length of each channel of the input.
     """
-    y = np.sum(np.diff(x, axis=0), axis=0)
-    return y
+
+    def __init__(self):
+        pass
+
+    def compute(self, x):
+        y = np.sum(np.diff(x, axis=0), axis=0)
+        return y
 
 
-def zc(x, thresh):
+class ZC:
     """
     Calculates the number of zero crossings in a signal, subject to a threshold
     for discarding noisy fluctuations above and below zero.
 
     Parameters
     ----------
-    x : ndarray
-        The raw signal(s) to calculate ZC for. If multidimensional, ZC is
-        calculated along columns.
-    thresh : float
+    thresh : float (default=0.0)
         The threshold for discriminating true zero crossings from those caused
         by noise.
-
-    Returns
-    -------
-    y : ndarray
-        The number of zero crossings in each channel of the input.
+    use_sm : bool (default=False)
+        Specifies if spectral moments should be used for the computation. This
+        is much faster, but the threshold is not taken into account, making it
+        potentially affected by noise.
     """
-    xrows, xcols = x.shape
-    y = np.zeros(xcols)
-    for i in range(xcols):
-        for j in range(1, xrows):
-            if ((x[j, i] > 0 and x[j-1, i] < 0) or
-                    (x[j, i] < 0 and x[j-1, i] > 0)):
-                if np.absolute(x[j, i] - x[j-1, i]) > thresh:
-                    y[i] += 1
-    return y
+
+    def __init__(self, thresh=0.0, use_sm=False):
+        self.thresh = thresh
+        self.use_sm = use_sm
+
+    def compute(self, x):
+        if self.use_sm:
+            y = np.sqrt(
+                SpectralMoment(2).compute(x) / SpectralMoment(0).compute(x))
+
+        else:
+            xrows, xcols = x.shape
+            y = np.zeros(xcols)
+            for i in range(xcols):
+                for j in range(1, xrows):
+                    if ((x[j, i] > 0 and x[j-1, i] < 0) or
+                            (x[j, i] < 0 and x[j-1, i] > 0)):
+                        if np.absolute(x[j, i] - x[j-1, i]) > self.thresh:
+                            y[i] += 1
+
+        return y
 
 
-def zc_sm(x):
-    """
-    Computes the number of zero crossings in a signal using spectral moments.
-    This version is much faster to compute than plain zc(), but there is no
-    threshold parameter.
-
-    Parameters
-    ----------
-    x : ndarray
-        The raw signal. ZC is computed for each column.
-    """
-    y = np.sqrt(spectral_moment(x, 2) / spectral_moment(x, 0))
-    return y
-
-
-def ssc(x, thresh):
+class SSC:
     """
     Calculates the number of slope sign changes in a signal, subject to a
     threshold for discarding noisy fluctuations.
 
     Parameters
     ----------
-    x : ndarray
-        The raw signal(s) to calculate SSC for. If multidimensional, SSC is
-        calculated along columns.
-    thresh : float
+    thresh : float (default=0.0)
         The threshold for discriminating true slope sign changes from those
         caused by noise.
-
-    Returns
-    -------
-    y : ndarray
-        The number of slope sign changes in each channel of the input.
+    use_sm : bool (deafult=False)
+        Specifies if spectral moments should be used for the computation. This
+        is much faster, but the threshold is not taken into account, making it
+        potentially affected by noise.
     """
-    xrows, xcols = x.shape
-    y = np.zeros(xcols)
-    for i in range(xcols):
-        for j in range(1, xrows-1):
-            if ((x[j, i] > x[j-1, i] and x[j, i] > x[j+1, i]) or
-                    (x[j, i] < x[j-1, i] and x[j, i] < x[j+1, i])):
-                if (np.absolute(x[j, i] - x[j-1, i]) > thresh or
-                        np.absolute(x[j, i] - x[j+1, i]) > thresh):
-                    y[i] += 1
-    return y
 
+    def __init__(self, thresh=0.0, use_sm=False):
+        self.thresh = thresh
+        self.use_sm = use_sm
 
-def ssc_sm(x):
-    """
-    Computes the number of slope sign changes (peaks) in a signal using
-    spectral moments. This version is much faster to compute than plain
-    ssc(), but there is no threshold parameter.
+    def compute(self, x):
+        if self.use_sm:
+            y = np.sqrt(
+                SpectralMoment(4).compute(x) / SpectralMoment(2).compute(x))
 
-    Parameters
-    ----------
-    x : ndarray
-        The raw signal. SSC is computed for each column.
-    """
-    y = np.sqrt(spectral_moment(x, 4) / spectral_moment(x, 2))
-    return y
-
-
-def spectral_moment(x, n):
-    """
-    Calculates the nth spectral moment of a signal.
-
-    Parameters
-    ----------
-    x : ndarray
-        The raw signal. If multidimensional, the moment is calculated for each
-        column.
-    n : int
-        The order of the moment to take. Should be even and >= 0.
-
-    Returns
-    -------
-    y : ndarray
-        The spectral moment of each channel of the input.
-    """
-    xrows, xcols = x.shape
-    y = np.zeros(xcols)
-
-    if n % 2 != 0:
+        else:
+            xrows, xcols = x.shape
+            y = np.zeros(xcols)
+            for i in range(xcols):
+                for j in range(1, xrows-1):
+                    if ((x[j, i] > x[j-1, i] and x[j, i] > x[j+1, i]) or
+                            (x[j, i] < x[j-1, i] and x[j, i] < x[j+1, i])):
+                        if (np.absolute(x[j, i]-x[j-1, i]) > self.thresh or
+                                np.absolute(x[j, i]-x[j+1, i]) > self.thresh):
+                            y[i] += 1
         return y
 
-    # special case, zeroth order moment is just the power
-    if n == 0:
-        y = np.sum(np.multiply(x, x), axis=0)
 
-    else:
-        y = spectral_moment(np.diff(x, int(n/2), axis=0), 0)
+class SpectralMoment:
+    """
+    Calculates the nth-order spectral moment.
 
-    return y
+    Parameters
+    ----------
+    n : int
+        The spectral moment order. Should be even and greater than or equal to
+        zero.
+    """
+
+    def __init__(self, n):
+        self.n = n
+
+    def compute(self, x):
+        xrows, xcols = x.shape
+        y = np.zeros(xcols)
+
+        if self.n % 2 != 0:
+            return y
+
+        # special case, zeroth order moment is just the power
+        if self.n == 0:
+            y = np.sum(np.multiply(x, x), axis=0)
+
+        else:
+            y = SpectralMoment(0).compute(np.diff(x, int(self.n/2), axis=0))
+
+        return y
 
 
-def khushaba_set(x, u=0):
+class KhushabaSet:
     """
     Calcuates a set of 5 features introduced by Khushaba et al. at ISCIT 2012.
-    They are:
+    (see reference [1]). They are:
         1. log of the 0th order spectral moment
         2. log of normalized 2nd order spectral moment (m2 / m0^u)
         3. log of normalized 4th order spectral moment (m4 / m0^(u+2))
@@ -177,30 +148,40 @@ def khushaba_set(x, u=0):
 
     Parameters
     ----------
-    x : ndarray
-        Raw signal. Each feature in the set is calculated for each column.
-    u : int, optional (default = 0)
+    u : int (default=0)
         Used in the exponent of m0 for normalizing higher-orer moments
+
+    References
+    ----------
+    .. [1] `R. N. Khushaba, L. Shi, and S. Kodagoda, "Time-dependent spectral
+        features for limb position invariant myoelectric pattern recognition,"
+        Communications and Information Technologies (ISCIT), 2012 International
+        Symposium on, 2012.`
     """
-    m0 = spectral_moment(x, 0)
-    m2 = spectral_moment(x, 2)
-    m4 = spectral_moment(x, 4)
-    S = m0 / np.sqrt(np.abs((m0-m2)*(m0-m4)))
-    IF = np.sqrt(m2**2 / (m0*m4))
-    WL = wl(x)
 
-    return np.hstack((
-        np.log(m0),
-        np.log(m2 / m0**2),
-        np.log(m4 / m0**4),
-        np.log(S),
-        np.log(IF / WL)))
+    def __init__(self, u=0):
+        self.u = u
+
+    def compute(self, x):
+        m0 = SpectralMoment(0).compute(x)
+        m2 = SpectralMoment(2).compute(x)
+        m4 = SpectralMoment(4).compute(x)
+        S = m0 / np.sqrt(np.abs((m0-m2)*(m0-m4)))
+        IF = np.sqrt(m2**2 / (m0*m4))
+
+        return np.hstack((
+            np.log(m0),
+            np.log(m2 / m0**2),
+            np.log(m4 / m0**4),
+            np.log(S),
+            np.log(IF / WL().compute(x))))
 
 
-def sampen(x, m, r):
+class SampEn:
     """
-    Calculates the sample entropy of time series data. See Richman and Moorman
-    2000. The basic idea is to take all possible m-length subsequences of the
+    Calculates the sample entropy of time series data. See reference [1].
+
+    The basic idea is to take all possible m-length subsequences of the
     time series and count the number of these subsequences whose Chebyshev
     distance from all other subsequences is less than the tolerance parameter,
     r (self-matches excluded). This is repeated for (m+1)-length subsequences,
@@ -213,39 +194,50 @@ def sampen(x, m, r):
 
     Parameters
     ----------
-    x : ndarray
-        Input data. Sample entropy is calculated for each column.
     m : int
         Length of sequences to compare (>1)
     r : float
-        Tolerance for counting matches. 
+        Tolerance for counting matches.
+
+    References
+    ----------
+    .. [1] `J. S. Richman and J. R. Moorman, "Physiological time series
+        analysis using approximate entropy and sample entropy," American
+        Journal of Physiology -- Heart and Circulatory Physiology, vol. 278
+        no. 6, 2000.`
     """
 
-    xrows, xcols = x.shape
-    y = np.zeros(xcols)
+    def __init__(self, m, r):
+        self.m = m
+        self.r = r
 
-    N = xrows
-    for c in range(xcols):
-        correl = np.zeros(2) + np.finfo(np.float).eps
+    def compute(self, x):
+        xrows, xcols = x.shape
+        y = np.zeros(xcols)
+        m = self.m
+        N = xrows
 
-        xmat = np.zeros((m+1, N-m+1))
-        for i in range(m):
-            xmat[i, :] = x[i:N-m+i+1, c]
-        # handle last row separately
-        xmat[m, :-1] = x[m:N, c]
-        xmat[-1, -1] = 10*np.max(xmat) # something that won't get matched
+        for c in range(xcols):
+            correl = np.zeros(2) + np.finfo(np.float).eps
 
-        for mc in [m, m+1]:
-            count = 0
-            for i in range(N-mc-1):
-                dist = np.max(
-                    np.abs(
-                        xmat[:mc, i+1:] - xmat[:mc, i][:, np.newaxis]), axis=0)
+            xmat = np.zeros((m+1, N-m+1))
+            for i in range(m):
+                xmat[i, :] = x[i:N-m+i+1, c]
+            # handle last row separately
+            xmat[m, :-1] = x[m:N, c]
+            xmat[-1, -1] = 10*np.max(xmat)  # something that won't get matched
 
-                count += np.sum(dist <= r)
+            for mc in [m, m+1]:
+                count = 0
+                for i in range(N-mc-1):
+                    dist = np.max(
+                        np.abs(xmat[:mc, i+1:] - xmat[:mc, i][:, np.newaxis]),
+                        axis=0)
 
-            correl[mc-m] = count
+                    count += np.sum(dist <= self.r)
 
-        y[c] = np.log(correl[0] / correl[1])
+                correl[mc-m] = count
 
-    return y
+            y[c] = np.log(correl[0] / correl[1])
+
+        return y
