dde94188d173f3986b114c20656277a8ffbfff07
Kenneth Lyons <ixjlyons@gmail.com>
2015-04-01 10:05:03 -0700

    Added more tests for DBVRController.

--- pygesture/tests/test_control.py
+++ pygesture/tests/test_control.py
@@ -47,6 +47,55 @@ class TestDBVRController(object):
         assert on_ramp > 0 and on_ramp < 1
         assert saturated == 1
 
+    def test_complex(self):
+        mapping = {0: 'no-contraction', 1: '1', 2: '2'}
+
+        controller = control.DBVRController(
+            mapping, ramp_length=5, boosts=1)
+
+        out = controller.process((1, 0))
+        assert out == 'no-contraction'
+        out = controller.process((1, 1))
+        assert out['1'] > 0
+        assert out['2'] == 0
+        out = controller.process((1, 2))
+        assert out['1'] == 0
+        assert out['2'] > 0
+
+    def test_ramp_length_1(self):
+        mapping = {0: '0', 1: '1'}
+
+        controller = control.DBVRController(
+            mapping, ramp_length=1, boosts=1)
+
+        out = controller.process((1, 0))
+        assert out == {'0': 1, '1': 0}
+        out = controller.process((1, 1))
+        assert out == {'0': 0, '1': 1}
+
+    def test_boosts(self):
+        mapping = {0: '0', 1: '1'}
+        boosts = {0: 2, 1: 1}
+        labels = [0]*7 + [1]*7
+        mav = [1]*len(labels)
+        data = zip(mav, labels)
+
+        controller = control.DBVRController(
+            mapping, ramp_length=5, boosts=boosts)
+
+        out = _run_controller(controller, data)
+        assert out[5]['0'] == boosts[0]
+        assert out[-1]['1'] == boosts[1]
+
+    def test_mav(self):
+        mapping = {0: '0', 1: '1'}
+
+        controller = control.DBVRController(
+            mapping, ramp_length=1, boosts=1)
+
+        out = controller.process((2, 0))
+        assert out == {'0': 2, '1': 0}
+
 
 def _run_controller(controller, data):
     out = []
