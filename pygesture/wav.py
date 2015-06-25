63bd630c25d3fe93e655d90f9ba51a2a148ae663
Kenneth Lyons <ixjlyons@gmail.com>
2015-06-24 18:13:02 -0700

    Forgot to add wav.py to previous commit.

---
+++ pygesture/wav.py
@@ -0,0 +1,45 @@
+import numpy as np
+import scipy.io.wavfile as siowav
+
+
+def write(filename, rate, data):
+    """
+    Writes recording data to file in WAV format. It is basically a convenience
+    wrapper around `scipy.io.wavfile.write` for handling normalized float data.
+
+    Paramters
+    ---------
+    filename : str
+        Path + file name to the file to write to.
+    rate : int
+        Sample rate in Hz.
+    data : ndarray
+        Data to write. For multi-channel recordings, the shape should be
+        (num_samples, num_channels).
+    """
+    data *= 32768
+    data = data.astype(np.int16, copy=False)
+    siowav.write(filename, rate, data)
+
+
+def read(filename):
+    """
+    Reads recording data from a WAV file. It is basically a convenience wrapper
+    around `scipy.io.wavfile.read` for getting float data.
+
+    Parameters
+    ----------
+    filename : str
+        Path + file name to the file to read from.
+
+    Returns
+    -------
+    rate : int
+        Sample rate.
+    data : ndarray
+        Float data (-1 to 1) from the file. Shape is (num_samples,
+        num_channels).
+    """
+    rate, data = siowav.read(filename)
+    data = data / 32768.0
+    return (rate, data)
