ece5bc4fd056a3e9c59b8c21b038077ee3ac49ea
Kenneth Lyons <ixjlyons@gmail.com>
2015-11-19 17:22:50 -0800

    Building up understanding of QGraphicsView/Scene/Item for cursor interface.

--- pygesture/cursor/cursor2d.py
+++ pygesture/cursor/cursor2d.py
@@ -4,10 +4,10 @@ from pygesture.ui.qt import QtCore, QtGui, QtWidgets
 
 class CursorInterface2D(QtWidgets.QGraphicsView):
 
-    left = -150
-    bottom = -150
-    width = 300
-    height = 300
+    initleft = -100
+    initbottom = initleft
+    initwidth = -initleft*2
+    initheight = -initleft*2
 
     def __init__(self, parent=None):
         super(CursorInterface2D, self).__init__(parent)
@@ -16,15 +16,18 @@ class CursorInterface2D(QtWidgets.QGraphicsView):
         self.init_border()
 
     def init_scene(self):
-        scene = QtWidgets.QGraphicsScene(self)
-        scene.setSceneRect(self.left, self.bottom, self.width, self.height)
+        scene = QtWidgets.QGraphicsScene()
+        scene.setSceneRect(self.initleft, self.initbottom,
+                           self.initwidth, self.initheight)
+        scene.setItemIndexMethod(QtWidgets.QGraphicsScene.NoIndex)
 
         self.setScene(scene)
         self.setRenderHint(QtGui.QPainter.Antialiasing)
+        self.setBackgroundBrush(QtCore.Qt.white)
 
     def init_border(self):
         rect = self.scene().sceneRect()
-        pen = QtGui.QPen(QtCore.Qt.red)
+        pen = QtGui.QPen(QtGui.QColor('#444444'))
         lines = [
             QtCore.QLineF(rect.topLeft(), rect.topRight()),
             QtCore.QLineF(rect.topLeft(), rect.bottomLeft()),
@@ -35,39 +38,103 @@ class CursorInterface2D(QtWidgets.QGraphicsView):
         for line in lines:
             self.scene().addLine(line, pen)
 
+    def map_coords(self, nx, ny):
+        return self.map_size(nx), -self.map_size(ny)
+
+    def map_size(self, size):
+        return size * (self.sceneRect().width()/2)
+
+    def resizeEvent(self, event):
+        self.fitInView(self.sceneRect(), QtCore.Qt.KeepAspectRatio)
+
 
 class Circle(QtWidgets.QGraphicsObject):
 
-    def __init__(self, diameter, startx=0, starty=0):
+    def __init__(self, diameter, color='#333333'):
         super(Circle, self).__init__()
 
         self.diameter = diameter
-        self.startx = startx
-        self.starty = starty
+
+        self._bounding_rect = QtCore.QRectF(-self.diameter/2, -self.diameter/2,
+                                            self.diameter, self.diameter)
+        p = QtGui.QPainterPath()
+        p.addEllipse(self._bounding_rect)
+        self._shape = p
+
+        self._color = None
+        self.color = color
+
+        self._norm_x = 0
+        self._norm_y = 0
 
     def boundingRect(self):
-        return QtCore.QRectF(0, 0, self.diameter, self.diameter)
+        return self._bounding_rect
+
+    def shape(self):
+        return self._shape
 
     def paint(self, painter, option, widget):
-        rect = self.boundingRect()
+        painter.setPen(QtCore.Qt.NoPen)
+        painter.setBrush(self._color)
 
-        if self.scene().collidingItems(self):
-            col = QtCore.Qt.red
-        else:
-            col = QtCore.Qt.green
+        painter.drawEllipse(self._bounding_rect)
 
-        painter.setPen(QtGui.QPen(col))
+    @property
+    def color(self):
+        return self._color
 
-        painter.drawEllipse(rect)
+    @color.setter
+    def color(self, value):
+        self._color = QtGui.QColor(value)
+        self.update()
+
+    def set_norm_pos(self, nx, ny):
+        self.norm_x = nx
+        self.norm_y = ny
+
+    @property
+    def norm_x(self):
+        return self._norm_x
+
+    @norm_x.setter
+    def norm_x(self, value):
+        self._norm_x = value
+        self.setX(self.scene().width()/2*self._norm_x)
+
+    @property
+    def norm_y(self):
+        return self._norm_y
+
+    @norm_y.setter
+    def norm_y(self, value):
+        self._norm_y = value
+        self.setY(-self.scene().width()/2*self._norm_y)
 
 
 if __name__ == '__main__':
     app = QtWidgets.QApplication([])
     dialog = QtWidgets.QDialog()
     layout = QtWidgets.QGridLayout()
+
     interface = CursorInterface2D()
 
-    layout.addWidget(CursorInterface2D())
+    target = Circle(interface.map_size(0.2), color='#672311')
+    target.setPos(*interface.map_coords(0.5, 0.5))
+    interface.scene().addItem(target)
+
+    cursor = Circle(interface.map_size(0.1), color='#219421')
+    cursor.setPos(*interface.map_coords(0, 0))
+    interface.scene().addItem(cursor)
+
+    def timerEvent():
+        cursor.set_norm_pos(cursor.norm_x+0.01, cursor.norm_y+0.01)
+
+    timer = QtCore.QTimer()
+    timer.timeout.connect(timerEvent)
+    timer.start(1000/33)
+
+
+    layout.addWidget(interface)
     dialog.setLayout(layout)
 
     dialog.show()
